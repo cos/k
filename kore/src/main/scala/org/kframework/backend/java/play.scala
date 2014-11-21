ac8187b8872fd8750163c00ef45b636ab58fc017
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-11-21 17:05:31 -0600

    java interop

--- kore/src/main/scala/org/kframework/backend/java/play.scala
+++ kore/src/main/scala/org/kframework/backend/java/play.scala
@@ -4,22 +4,23 @@ import collection._
 import org.kframework._
 import kore._
 import KORE._
+import JavaConverters._
 
 trait Context {
   val productionForLabel: Map[String, Set[outer.SyntaxProduction]]
 }
 
-class KLabel(val name: String)(implicit context: Context) extends kore.KLabel {
+class KLabel(val name: String)(implicit context: Context) extends kore.KLabel with KLabelJavaInterop {
   import context._
 
-  lazy val productions = productionForLabel(name)
+  lazy val productionsS = productionForLabel(name)
 
   lazy val isSortPredicate = name.startsWith("is")
   lazy val predicateSort = if (isSortPredicate) Sort(name.replace("is", "")) else None
-  lazy val isFunction = isSortPredicate || (productions exists { _.att.contains("function") })
-  lazy val isPattern = productions exists { _.att.contains("pattern") }
+  lazy val isFunction = isSortPredicate || (productionsS exists { _.att.contains("function") })
+  lazy val isPattern = productionsS exists { _.att.contains("pattern") }
 
-  lazy val smtLib: Option[String] = productions map { _.att.get("smtlib") } reduce {
+  lazy val smtLib: Option[String] = productionsS map { _.att.get("smtlib") } reduce {
     (a, b) => if (a == b) a else throw new RuntimeException("Different patterns")
   } match {
     case KToken(StringToken, v, _) => Some(v.s)
@@ -32,6 +33,11 @@ class KLabel(val name: String)(implicit context: Context) extends kore.KLabel {
   lazy val cachedHashCode: Int = super.hashCode()
   override def hashCode() = cachedHashCode
   def readResolve(): Object = KLabel(this.name)
+}
+
+trait KLabelJavaInterop {
+  self: KLabel =>
+  lazy val productions = productionsS.asJava
 }
 
 object KLabel {
